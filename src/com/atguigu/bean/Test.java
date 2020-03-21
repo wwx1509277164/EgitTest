084549d6902a6b076bc4dfd491a11b5262b3692d
zyy1509277164 <2392340989@qq.com>
2020-03-22 07:33:19 +0800

    第一次修改

--- src/com/atguigu/bean/Test.java
+++ src/com/atguigu/bean/Test.java
@@ -3,5 +3,7 @@ package com.atguigu.bean;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("fsdaf爱你范德萨发");
+		System.out.println("你给我小啊giao");
+		System.out.println("修改的第一次");
 	}
 }
