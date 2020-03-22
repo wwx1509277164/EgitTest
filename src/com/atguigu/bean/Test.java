66db3fe67180bac672418b468ca00901800b5d6d
wwx1509277164 <1509277164@qq.com>
2020-03-22 08:13:37 +0800

    第二次修改

--- src/com/atguigu/bean/Test.java
+++ src/com/atguigu/bean/Test.java
@@ -4,6 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("fsdaf爱你范德萨发");
 		
-		System.out.println("你给我弟小啊giao");
+		System.out.println("你给我弟小啊ssgiao");
 	}
 }
