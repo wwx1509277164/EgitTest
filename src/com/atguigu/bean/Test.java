c9b94a9246f111397d642fc710d72b502e439e0d
wwx1509277164 <1509277164@qq.com>
2020-03-22 08:30:27 +0800

    第三次修改

---
+++ src/com/atguigu/bean/Test.java
@@ -0,0 +1,12 @@
+package com.atguigu.bean;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("fsdaf爱你范德萨发");
+		System.out.println("回来吧");
+		System.out.println("你给我弟fsdaf小啊ssgiao");
+		System.out.println("你给我小啊giao");
+		System.out.println("是否撒的");
+		System.out.println("修改的第一次");
+	}
+}
